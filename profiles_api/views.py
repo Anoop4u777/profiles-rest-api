06365402f1d23c632ebcf43eb270e4c61123597c
Anoop Krishnan.R <anoop4u777@gmail.com>
2020-11-19 22:32:19 +0530

    First API View.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+
+        an_apiview = [
+            'Uses HTTP functions as methods:-',
+            'GET',
+            'POST',
+            'PUT',
+            'PATCH',
+            'DELETE',
+        ]
+
+        return Response({'Message': 'Hello!!!', 'an_apiview': an_apiview})
