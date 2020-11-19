55f0043de2e993343dba233e1bcc3d585d8b19d5
Anoop Krishnan.R <anoop4u777@gmail.com>
2020-11-19 23:25:00 +0530

    Serializers and rest of APIView.

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
 
     def get(self, request, format=None):
 
@@ -16,3 +20,33 @@ class HelloApiView(APIView):
         ]
 
         return Response({'Message': 'Hello!!!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a post without a model"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            """Below the serializer.validated_data passes orderedDict(list[(tuples)])"""
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name} !!!'
+            return Response({'Message': message}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Updates the entire object.
+            Example :-
+            If there is 2 fields in a model a,b
+            then if u put by only giving b the PUT
+            will remove the value of a ie, what put does it overwrites an
+            object with the key values that you pass during the using of PUT.
+        """
+        return Response({'Message':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Updates an object partially"""
+        return Response({'Message':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an entire object"""
+        return Response({'Message':'DELETE'})
